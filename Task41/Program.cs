519a893664bf3d5a28613f80481f68f4fa3f81ab
danil <netrow@gmail.com>
2023-01-27 01:15:12 +0500

    Добавлены домашние задачи

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите колличество чисел: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+if (numbers > 0)
+{
+    int[] creatEnterArray = CreatEnterArray(numbers);
+    PrintArray(creatEnterArray);
+    int positiveNumbers = PositiveNumbers(creatEnterArray);
+    Console.Write($"Положительные числа: {positiveNumbers}");
+}
+else Console.WriteLine("Введите другое число: ");
+
+int[] CreatEnterArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Какие числа хотите проверить? : ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + " ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine();
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Task45/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] arrayOrigin = CreateArrayRndInt(10, -3, 5);
+PrintArray(arrayOrigin);
+int[] arrayCopy = CopyArray(arrayOrigin); // передача массива 
+arrayCopy[0]=30; // проверка что создалась копия у которого первый элемент 30
+PrintArray(arrayCopy);
+
+int[] CopyArray (int[] arr) // передается массив 
+{
+    int[] result = new int[arr.Length]; // создание нового массива, с длинной массива которого передали 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i]; // создали новый массив и прировнаяли его к старому
+    }
+    return result;
+}
+
+int[] CreateArrayRndInt (int size, int min, int max) 
+{
+    int[] arr = new int[size]; 
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        arr[i] = rnd.Next(min,max + 1); 
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) 
+{
+   Console.Write("[");
+   for (int i = 0; i < arr.Length; i++) 
+    {
+       if(i < arr.Length - 1) Console.Write(arr[i] + ","); 
+       else Console.Write(arr[i]);
+    }   
+    Console.Write("]"); 
+}
