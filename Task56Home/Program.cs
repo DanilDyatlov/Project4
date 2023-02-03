ca3bfa686b7431ea0dd537c60b53af14eec1a840
danil <netrow@gmail.com>
2023-02-03 20:59:08 +0500

    Добавлено ДЗ

---
+++ Task56Home/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] newMatrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return newMatrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int IndexRowMinSum(int[,] matrix)
+{
+    int result = 0;
+    int minSum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j]; // в сумму кладем число из массива 
+        }
+        if (sum < minSum || minSum == 0)
+        {
+            minSum = sum;
+            result = i; // цифра строки в результат 
+        }
+    }
+    return result;
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 5, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+int indexRowMinSum = IndexRowMinSum(array2d);
+int number = indexRowMinSum + 1; // для пользователя указываем реальную строку 
+Console.WriteLine($"Наименьшая сумма элементов в {number} строке.");
---
+++ Task58Home/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] multiplyMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    if (firstMatrix.GetLength(1) == secondMatrix.GetLength(0)) 
+    {
+        for (int i = 0; i < firstMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < secondMatrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < firstMatrix.GetLength(1); k++)
+                {
+                    multiplyMatrix[i, j] = multiplyMatrix[i, j] + firstMatrix[i, k] * secondMatrix[k, j];
+                }
+            }
+        }
+    }
+    else Console.WriteLine("Невозможно выполнить, так как размеры массивов не равны.");
+    return multiplyMatrix;
+}
+
+int[,] matrix1 = CreateMatrixRndInt(4, 5, 1, 9);
+PrintMatrix(matrix1);
+Console.WriteLine();
+int[,] matrix2 = CreateMatrixRndInt(5, 3, 1, 9);
+PrintMatrix(matrix2);
+
+Console.WriteLine();
+int[,] multiMatrix = MultiplyMatrix(matrix1, matrix2);
+PrintMatrix(multiMatrix);
